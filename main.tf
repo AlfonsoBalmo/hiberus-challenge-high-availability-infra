c8974beb26c9b3eb65583693b3b58a67e7f537cc
Alfonso <alfonso.balmori@skydropx.com>
2024-06-12 13:47:18 -0600

    database version

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_db_instance" "default" {
 
 resource "aws_lambda_function" "my_lambda" {
   function_name = "hiberus-lambda"
-  role          = aws_iam_role.lambda_exec.arn
+  role          = "arn:aws:iam::YOUR_ACCOUNT_ID:role/lambda_exec_role"
   handler       = "handler.lambda_handler"
   runtime       = "python3.8"
   filename      = "lambda_function.zip"
@@ -32,39 +32,3 @@ resource "aws_lambda_function" "my_lambda" {
     }
   }
 }
-
-resource "aws_iam_role" "lambda_exec" {
-  name = "lambda_exec_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-
-  inline_policy {
-    name = "lambda_policy"
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Action = [
-            "rds:*",
-            "logs:*",
-            "cloudwatch:*"
-          ]
-          Effect = "Allow"
-          Resource = "*"
-        }
-      ]
-    })
-  }
-}
-
