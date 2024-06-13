7ceef5ccb3a857bf4dec0f9c445e1f76b30ccdbb
Alfonso <alfonso.balmori@skydropx.com>
2024-06-12 22:28:32 -0600

    fix script

--- handler.py
+++ handler.py
@@ -10,14 +10,26 @@ logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     logger.info("Received event: %s", json.dumps(event))
+
+    # Imprimir las variables de entorno
+    db_host = os.environ.get('DB_HOST')
+    db_user = os.environ.get('DB_USER')
+    db_password = os.environ.get('DB_PASSWORD')
+    db_name = os.environ.get('DB_NAME')
+
+    logger.info(f"DB_HOST: {db_host}")
+    logger.info(f"DB_USER: {db_user}")
+    logger.info(f"DB_PASSWORD: {db_password}")
+    logger.info(f"DB_NAME: {db_name}")
+
     connection = None
     try:
         # Establecer la conexión con la base de datos utilizando variables de entorno
         connection = pymysql.connect(
-            host=os.environ['DB_HOST'],
-            user=os.environ['DB_USER'],
-            password=os.environ['DB_PASSWORD'],
-            db=os.environ['DB_NAME'],
+            host=db_host,
+            user=db_user,
+            password=db_password,
+            db=db_name,
             cursorclass=DictCursor
         )
         logger.info("Connection to RDS established")
